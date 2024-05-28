b13964b04d8305fe474bc90a1aa9e5143d78b1a7
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-05-28 12:31:54 -0700

    added the getter function to get the conversations and added the components for conversationdId folder

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as conversation from "../conversation.js";
 import type * as conversations from "../conversations.js";
 import type * as request from "../request.js";
 import type * as requests from "../requests.js";
@@ -28,6 +29,7 @@ import type * as user from "../user.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  conversation: typeof conversation;
   conversations: typeof conversations;
   request: typeof request;
   requests: typeof requests;
